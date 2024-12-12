1efd6060efbed2ca69cf8d4f458cb79c030f6c67
shbotirov227 <shbotirov227@gmail.com>
2024-12-12 17:16:25 +0500

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { Theme } from "@radix-ui/themes";
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Theme>
+      <App />
+    </Theme>
+  </StrictMode>,
+)
